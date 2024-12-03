8e76367ba4778bac3bb6e59296f2b5715b833b3d
Sami Hosni <samy.hosni@gmail.com>
2024-12-03 18:11:21 +0100

    debug 29

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -105,7 +105,7 @@ pipeline {
                             groupId: pom.groupId,
                             version: pom.version,
                             repository: NEXUS_REPOSITORY,
-                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            credentialsId: NEXUS_CREDENTIAL,
                             artifacts: [[artifactId: pom.artifactId, classifier: '', file: artifactPath, type: pom.packaging]]
                     )
                 }
